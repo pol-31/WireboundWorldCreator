f8bdb4197fa7c1e3363c50a917188105f384f4fb
pol-31 <tsrkpashkevich@gmail.com>
2024-08-10 23:58:15 +0300

    move all vbo data to single header src/common/Vbos.h (ui & text sprites data, instanced data)

--- src/common/Vbos.h
+++ src/common/Vbos.h
@@ -1,11 +1,22 @@
-#ifndef WIREBOUNDWORLDCREATOR_SRC_VBO_UIDATAMAIN_H_
-#define WIREBOUNDWORLDCREATOR_SRC_VBO_UIDATAMAIN_H_
+#ifndef WIREBOUNDWORLDCREATOR_SRC_COMMON_VBOS_H_
+#define WIREBOUNDWORLDCREATOR_SRC_COMMON_VBOS_H_
 
 #include <array>
 
-enum class UiVboDataMainId;
+#include <cinttypes>
 
-inline consteval std::size_t GetUiDataMainOffset(UiVboDataMainId id);
+struct UiData {
+  int32_t id;
+  std::size_t vbo_offset;
+};
+
+enum class UiVboDataMainId;
+enum class UiVboDataTextId;
+enum class UiVboDataInstancedId;
+
+inline constexpr std::size_t GetUiDataMainOffset(UiVboDataMainId id);
+inline constexpr std::size_t GetUiDataTextOffset(UiVboDataTextId id);
+inline constexpr std::size_t GetUiDataInstancedOffset(UiVboDataInstancedId id);
 
 /// related to kUiVboDataMain
 enum class UiVboDataMainId {
@@ -88,10 +99,99 @@ enum class UiVboDataMainId {
   kTilesPreview,
 };
 
-inline consteval std::size_t GetUiDataMainOffset(UiVboDataMainId id) {
+enum class UiVboDataTextId {
+  kMode,
+  kVision,
+  kTerrain,
+  kWater,
+  kRoads,
+  kFences,
+  kPlacement,
+  kObject,
+  kBiome,
+  kTiles,
+  kShaders,
+  kWirebound,
+  kSmooth,
+  kUpdateModesMaps,
+  kAdjustSize,
+  kAdjustFalloff,
+  kAddNew,
+  kRemoveSelected,
+  kBakeAsALake,
+  kBakeAsARiver,
+  kBakeAsAWaterfall,
+  kBakeAsAnAsphaltRoad,
+  kBakeAsAGravelRoad,
+  kBakeAsASoilRoad,
+  kBakeAsAPicketFence,
+  kBakeAsAChainLinkedFence,
+  kBakeAsAWoodenFence,
+  kPreviousSlot,
+  kNextSlot,
+  kPlaceTrees,
+  kPlaceBushes,
+  kPlaceTallGrass,
+  kPlaceUndergrowth,
+  kSetDrawColorToWhite,
+  kSetDrawColorToLightGrey,
+  kSetDrawColorToGrey,
+  kSetDrawColorToDarkGrey,
+  kSetDrawColorToBlack,
+  kPreviousPage,
+  kNextPage,
+  kPreviousRow,
+  kNextRow,
+  kPreviousColumn,
+  kNextColumn,
+  kCurrentTile,
+  kBakingInProcess,
+  kObjectWasNotBakedSoRemoved,
+  kSaving,
+  kLoading,
+  kUnableToOpenTheFile,
+  kUnableToSaveTheFile,
+};
+
+enum class UiVboDataInstancedId {
+  kObjects,
+  kBiomes,
+  kTiles,
+};
+
+inline constexpr std::size_t GetUiDataMainOffset(UiVboDataMainId id) {
   // for each Ui component 16 floats
   // (4 2d points with position and tex coords)
-  return static_cast<std::size_t>(id) * 16;
+  return static_cast<std::size_t>(id) * 4; // TODO: 4?
+}
+
+inline constexpr std::size_t GetUiDataTextOffset(UiVboDataTextId id) {
+  return static_cast<std::size_t>(id) * 2; // TODO: or 8?
+}
+
+inline constexpr std::size_t GetUiDataInstancedOffset(
+    UiVboDataInstancedId id) {
+  switch(id) {
+    case UiVboDataInstancedId::kObjects:
+      return 0;
+    case UiVboDataInstancedId::kBiomes:
+      return 8 * 4 * 8;
+    case UiVboDataInstancedId::kTiles:
+      return (8 * 4 + 8) * 8;
+  }
+}
+
+UiData GetUiData(UiVboDataInstancedId btn_type) {
+  return {static_cast<int>(btn_type), GetUiDataInstancedOffset(btn_type)};
+}
+
+
+UiData GetUiData(UiVboDataTextId btn_type) {
+  return {static_cast<int>(btn_type), GetUiDataTextOffset(btn_type)};
+}
+
+UiData GetUiData(UiVboDataMainId btn_type) {
+  return {static_cast<int>(btn_type), GetUiDataMainOffset(btn_type)};
 }
 
 namespace details {
@@ -528,6 +628,159 @@ inline constexpr std::array<float, 65 * 16> kUiVboDataMain = {
     0.1f, 0.4f, 0.0f, 1.0f,
 };
 
+// tex coords were generated with https://github.com/pol-31/WireboundTextBaker;
+inline constexpr std::array<float, 400> kUiVboDataText = {
+    0.3456f, 0.1264f, 0.3456f, 0.1888f, 0.2688f, 0.1264f, 0.2688f, 0.1888f,
+    0.8768f, 0.1888f, 0.8768f, 0.2512f, 0.7744f, 0.1888f, 0.7744f, 0.2512f,
+    0.4288f, 0.1888f, 0.4288f, 0.2512f, 0.3008f, 0.1888f, 0.3008f, 0.2512f,
+    0.9728f, 0.1888f, 0.9728f, 0.2512f, 0.8768f, 0.1888f, 0.8768f, 0.2512f,
+    0.0960f, 0.1264f, 0.0960f, 0.1888f, 0.0f,    0.1264f, 0.0f,    0.1888f,
+    0.5440f, 0.1888f, 0.5440f, 0.2512f, 0.4288f, 0.1888f, 0.4288f, 0.2512f,
+    0.9984f, 0.6256f, 0.9984f, 0.6880f, 0.8320f, 0.6256f, 0.8320f, 0.6880f,
+    0.6592f, 0.1888f, 0.6592f, 0.2512f, 0.5440f, 0.1888f, 0.5440f, 0.2512f,
+    0.1856f, 0.1264f, 0.1856f, 0.1888f, 0.0960f, 0.1264f, 0.0960f, 0.1888f,
+    0.2688f, 0.1264f, 0.2688f, 0.1888f, 0.1856f, 0.1264f, 0.1856f, 0.1888f,
+    0.9952f, 0.3136f, 0.9952f, 0.3760f, 0.8608f, 0.3136f, 0.8608f, 0.3760f,
+    0.5824f, 0.2512f, 0.5824f, 0.3136f, 0.4032f, 0.2512f, 0.4032f, 0.3136f,
+    0.7744f, 0.1888f, 0.7744f, 0.2512f, 0.6592f, 0.1888f, 0.6592f, 0.2512f,
+    0.6752f, 0.5008f, 0.6752f, 0.5632f, 0.3520f, 0.5008f, 0.3520f, 0.5632f,
+    0.4032f, 0.2512f, 0.4032f, 0.3136f, 0.2016f, 0.2512f, 0.2016f, 0.3136f,
+    0.5152f, 0.3760f, 0.5152f, 0.4384f, 0.2624f, 0.3760f, 0.2624f, 0.4384f,
+    0.3008f, 0.1888f, 0.3008f, 0.2512f, 0.1664f, 0.1888f, 0.1664f, 0.2512f,
+    0.9952f, 0.5632f, 0.9952f, 0.6256f, 0.7136f, 0.5632f, 0.7136f, 0.6256f,
+    0.2624f, 0.3760f, 0.2624f, 0.4384f, 0.0f,    0.3760f, 0.0f,    0.4384f,
+    0.5696f, 0.4384f, 0.5696f, 0.5008f, 0.2880f, 0.4384f, 0.2880f, 0.5008f,
+    0.7136f, 0.5632f, 0.7136f, 0.6256f, 0.3616f, 0.5632f, 0.3616f, 0.6256f,
+    0.9536f, 0.8752f, 0.9536f, 0.9376f, 0.5184f, 0.8752f, 0.5184f, 0.9376f,
+    0.9856f, 0.9376f, 0.9856f, 1.0f,    0.5888f, 0.9376f, 0.5888f, 1.0f,
+    0.3520f, 0.5008f, 0.3520f, 0.5632f, 0.0f,    0.5008f, 0.0f,    0.5632f,
+    0.8320f, 0.6256f, 0.8320f, 0.6880f, 0.4160f, 0.6256f, 0.4160f, 0.6880f,
+    0.5184f, 0.8128f, 0.5184f, 0.8752f, 0.0f,    0.8128f, 0.0f,    0.8752f,
+    0.4224f, 0.6880f, 0.4224f, 0.7504f, 0.0f,    0.6880f, 0.0f,    0.7504f,
+    0.9888f, 0.3760f, 0.9888f, 0.4384f, 0.7552f, 0.3760f, 0.7552f, 0.4384f,
+    0.1664f, 0.1888f, 0.1664f, 0.2512f, 0.0f,    0.1888f, 0.0f,    0.2512f,
+    0.8608f, 0.3136f, 0.8608f, 0.3760f, 0.6560f, 0.3136f, 0.6560f, 0.3760f,
+    0.2208f, 0.3136f, 0.2208f, 0.3760f, 0.0f,    0.3136f, 0.0f,    0.3760f,
+    0.2880f, 0.4384f, 0.2880f, 0.5008f, 0.0f,    0.4384f, 0.0f,    0.5008f,
+    0.9920f, 0.5008f, 0.9920f, 0.5632f, 0.6752f, 0.5008f, 0.6752f, 0.5632f,
+    0.9472f, 0.8128f, 0.9472f, 0.8752f, 0.5184f, 0.8128f, 0.5184f, 0.8752f,
+    0.5184f, 0.8752f, 0.5184f, 0.9376f, 0.0f,    0.8752f, 0.0f,    0.9376f,
+    0.4160f, 0.6256f, 0.4160f, 0.6880f, 0.0f,    0.6256f, 0.0f,    0.6880f,
+    0.5120f, 0.7504f, 0.5120f, 0.8128f, 0.0f,    0.7504f, 0.0f,    0.8128f,
+    0.9408f, 0.7504f, 0.9408f, 0.8128f, 0.5120f, 0.7504f, 0.5120f, 0.8128f,
+    0.7552f, 0.3760f, 0.7552f, 0.4384f, 0.5152f, 0.3760f, 0.5152f, 0.4384f,
+    0.7552f, 0.2512f, 0.7552f, 0.3136f, 0.5824f, 0.2512f, 0.5824f, 0.3136f,
+    0.4416f, 0.3136f, 0.4416f, 0.3760f, 0.2208f, 0.3136f, 0.2208f, 0.3760f,
+    0.9920f, 0.4384f, 0.9920f, 0.5008f, 0.8384f, 0.4384f, 0.8384f, 0.5008f,
+    0.8384f, 0.4384f, 0.8384f, 0.5008f, 0.5696f, 0.4384f, 0.5696f, 0.5008f,
+    0.2016f, 0.2512f, 0.2016f, 0.3136f, 0.0f,    0.2512f, 0.0f,    0.3136f,
+    0.6560f, 0.3136f, 0.6560f, 0.3760f, 0.4416f, 0.3136f, 0.4416f, 0.3760f,
+    0.3616f, 0.5632f, 0.3616f, 0.6256f, 0.0f,    0.5632f, 0.0f,    0.6256f,
+    0.5888f, 0.9376f, 0.5888f, 1.0f,    0.0f,    0.9376f, 0.0f,    1.0f,
+    0.9984f, 0.6880f, 0.9984f, 0.7504f, 0.8416f, 0.6880f, 0.8416f, 0.7504f,
+    0.9248f, 0.2512f, 0.9248f, 0.3136f, 0.7552f, 0.2512f, 0.7552f, 0.3136f,
+    0.8416f, 0.6880f, 0.8416f, 0.7504f, 0.4224f, 0.6880f, 0.4224f, 0.7504f,
+};
+
+/// here position instanced data (only for Modes "Objects", "Biomes", "Tiles")
+inline constexpr std::array<float, (8 * 4 + 8 + 6 * 6) * 8>
+    kUiVboDataInstanced = {
+        // Objects slots (8 x 4)
+        0.4f, 0.6f, 0.4f, 0.8f, 0.2f, 0.6f, 0.2f, 0.8f,
+        0.6f, 0.6f, 0.6f, 0.8f, 0.4f, 0.6f, 0.4f, 0.8f,
+        0.8f, 0.6f, 0.8f, 0.8f, 0.6f, 0.6f, 0.6f, 0.8f,
+        1.0f, 0.6f, 1.0f, 0.8f, 0.8f, 0.6f, 0.8f, 0.8f,
+
+        0.4f, 0.4f, 0.4f, 0.6f, 0.2f, 0.4f, 0.2f, 0.6f,
+        0.6f, 0.4f, 0.6f, 0.6f, 0.4f, 0.4f, 0.4f, 0.6f,
+        0.8f, 0.4f, 0.8f, 0.6f, 0.6f, 0.4f, 0.6f, 0.6f,
+        1.0f, 0.4f, 1.0f, 0.6f, 0.8f, 0.4f, 0.8f, 0.6f,
+
+        0.4f, 0.2, 0.4f, 0.4f, 0.2f, 0.2f, 0.2f, 0.4f,
+        0.6f, 0.2, 0.6f, 0.4f, 0.4f, 0.2f, 0.4f, 0.4f,
+        0.8f, 0.2, 0.8f, 0.4f, 0.6f, 0.2f, 0.6f, 0.4f,
+        1.0f, 0.2, 1.0f, 0.4f, 0.8f, 0.2f, 0.8f, 0.4f,
+
+        0.4f, 0.0f, 0.4f, 0.2f, 0.2f, 0.0f, 0.2f, 0.2f,
+        0.6f, 0.0f, 0.6f, 0.2f, 0.4f, 0.0f, 0.4f, 0.2f,
+        0.8f, 0.0f, 0.8f, 0.2f, 0.6f, 0.0f, 0.6f, 0.2f,
+        1.0f, 0.0f, 1.0f, 0.2f, 0.8f, 0.0f, 0.8f, 0.2f,
+
+        0.4f, -0.2f, 0.4f, 0.0f, 0.2f, -0.2f, 0.2f, 0.0f,
+        0.6f, -0.2f, 0.6f, 0.0f, 0.4f, -0.2f, 0.4f, 0.0f,
+        0.8f, -0.2f, 0.8f, 0.0f, 0.6f, -0.2f, 0.6f, 0.0f,
+        1.0f, -0.2f, 1.0f, 0.0f, 0.8f, -0.2f, 0.8f, 0.0f,
+
+        0.4f, -0.4f, 0.4f, -0.2f, 0.2f, -0.4f, 0.2f, -0.2f,
+        0.6f, -0.4f, 0.6f, -0.2f, 0.4f, -0.4f, 0.4f, -0.2f,
+        0.8f, -0.4f, 0.8f, -0.2f, 0.6f, -0.4f, 0.6f, -0.2f,
+        1.0f, -0.4f, 1.0f, -0.2f, 0.8f, -0.4f, 0.8f, -0.2f,
+
+        0.4f, -0.6f, 0.4f, -0.4f, 0.2f, -0.6f, 0.2f, -0.4f,
+        0.6f, -0.6f, 0.6f, -0.4f, 0.4f, -0.6f, 0.4f, -0.4f,
+        0.8f, -0.6f, 0.8f, -0.4f, 0.6f, -0.6f, 0.6f, -0.4f,
+        1.0f, -0.6f, 1.0f, -0.4f, 0.8f, -0.6f, 0.8f, -0.4f,
+
+        0.4f, -0.8f, 0.4f, -0.6f, 0.2f, -0.8f, 0.2f, -0.6f,
+        0.6f, -0.8f, 0.6f, -0.6f, 0.4f, -0.8f, 0.4f, -0.6f,
+        0.8f, -0.8f, 0.8f, -0.6f, 0.6f, -0.8f, 0.6f, -0.6f,
+        1.0f, -0.8f, 1.0f, -0.6f, 0.8f, -0.8f, 0.8f, -0.6f,
+
+        // Biomes slots (8)
+        1.0f, 0.6f, 1.0f, 0.8f, 0.2f, 0.6f, 0.2f, 0.8f,
+        1.0f, 0.4f, 1.0f, 0.6f, 0.2f, 0.4f, 0.2f, 0.6f,
+        1.0f, 0.2, 1.0f, 0.4f, 0.2f, 0.2f, 0.2f, 0.4f,
+        1.0f, 0.0f, 1.0f, 0.2f, 0.2f, 0.0f, 0.2f, 0.2f,
+
+        1.0f, -0.2f, 1.0f, 0.0f, 0.2f, -0.2f, 0.2f, 0.0f,
+        1.0f, -0.4f, 1.0f, -0.2f, 0.2f, -0.4f, 0.2f, -0.2f,
+        1.0f, -0.6f, 1.0f, -0.4f, 0.2f, -0.6f, 0.2f, -0.4f,
+        1.0f, -0.8f, 1.0f, -0.6f, 0.2f, -0.8f, 0.2f, -0.6f,
+
+        // Tiles slots (6 x 6)
+        -0.8f, 0.2f, -0.8f, 0.3f, -0.9f, 0.2f, -0.9f, 0.3f,
+        -0.7f, 0.2f, -0.7f, 0.3f, -0.8f, 0.2f, -0.8f, 0.3f,
+        -0.6f, 0.2f, -0.6f, 0.3f, -0.7f, 0.2f, -0.7f, 0.3f,
+        -0.5f, 0.2f, -0.5f, 0.3f, -0.6f, 0.2f, -0.6f, 0.3f,
+        -0.4f, 0.2f, -0.4f, 0.3f, -0.5f, 0.2f, -0.5f, 0.3f,
+        -0.3f, 0.2f, -0.3f, 0.3f, -0.4f, 0.2f, -0.4f, 0.3f,
+
+        -0.8f, 0.1f, -0.8f, 0.2f, -0.9f, 0.1f, -0.9f, 0.2f,
+        -0.7f, 0.1f, -0.7f, 0.2f, -0.8f, 0.1f, -0.8f, 0.2f,
+        -0.6f, 0.1f, -0.6f, 0.2f, -0.7f, 0.1f, -0.7f, 0.2f,
+        -0.5f, 0.1f, -0.5f, 0.2f, -0.6f, 0.1f, -0.6f, 0.2f,
+        -0.4f, 0.1f, -0.4f, 0.2f, -0.5f, 0.1f, -0.5f, 0.2f,
+        -0.3f, 0.1f, -0.3f, 0.2f, -0.4f, 0.1f, -0.4f, 0.2f,
+
+        -0.8f, 0.0f, -0.8f, 0.1f, -0.9f, 0.0f, -0.9f, 0.1f,
+        -0.7f, 0.0f, -0.7f, 0.1f, -0.8f, 0.0f, -0.8f, 0.1f,
+        -0.6f, 0.0f, -0.6f, 0.1f, -0.7f, 0.0f, -0.7f, 0.1f,
+        -0.5f, 0.0f, -0.5f, 0.1f, -0.6f, 0.0f, -0.6f, 0.1f,
+        -0.4f, 0.0f, -0.4f, 0.1f, -0.5f, 0.0f, -0.5f, 0.1f,
+        -0.3f, 0.0f, -0.3f, 0.1f, -0.4f, 0.0f, -0.4f, 0.1f,
+
+        -0.8f, -0.1f, -0.8f, 0.0f, -0.9f, -0.1f, -0.9f, 0.0f,
+        -0.7f, -0.1f, -0.7f, 0.0f, -0.8f, -0.1f, -0.8f, 0.0f,
+        -0.6f, -0.1f, -0.6f, 0.0f, -0.7f, -0.1f, -0.7f, 0.0f,
+        -0.5f, -0.1f, -0.5f, 0.0f, -0.6f, -0.1f, -0.6f, 0.0f,
+        -0.4f, -0.1f, -0.4f, 0.0f, -0.5f, -0.1f, -0.5f, 0.0f,
+        -0.3f, -0.1f, -0.3f, 0.0f, -0.4f, -0.1f, -0.4f, 0.0f,
+
+        -0.8f, -0.2f, -0.8f, -0.1f, -0.9f, -0.2f, -0.9f, -0.1f,
+        -0.7f, -0.2f, -0.7f, -0.1f, -0.8f, -0.2f, -0.8f, -0.1f,
+        -0.6f, -0.2f, -0.6f, -0.1f, -0.7f, -0.2f, -0.7f, -0.1f,
+        -0.5f, -0.2f, -0.5f, -0.1f, -0.6f, -0.2f, -0.6f, -0.1f,
+        -0.4f, -0.2f, -0.4f, -0.1f, -0.5f, -0.2f, -0.5f, -0.1f,
+        -0.3f, -0.2f, -0.3f, -0.1f, -0.4f, -0.2f, -0.4f, -0.1f,
+
+        -0.8f, -0.3f, -0.8f, -0.2f, -0.9f, -0.3f, -0.9f, -0.2f,
+        -0.7f, -0.3f, -0.7f, -0.2f, -0.8f, -0.3f, -0.8f, -0.2f,
+        -0.6f, -0.3f, -0.6f, -0.2f, -0.7f, -0.3f, -0.7f, -0.2f,
+        -0.5f, -0.3f, -0.5f, -0.2f, -0.6f, -0.3f, -0.6f, -0.2f,
+        -0.4f, -0.3f, -0.4f, -0.2f, -0.5f, -0.3f, -0.5f, -0.2f,
+        -0.3f, -0.3f, -0.3f, -0.2f, -0.4f, -0.3f, -0.4f, -0.2f,
+};
+
 } // namespace details
 
-#endif  // WIREBOUNDWORLDCREATOR_SRC_VBO_UIDATAMAIN_H_
+#endif  // WIREBOUNDWORLDCREATOR_SRC_COMMON_VBOS_H_
--- src/vbos/AllVbos.h
+++
@@ -1,8 +0,0 @@
-#ifndef WIREBOUNDWORLDCREATOR_SRC_VBOS_ALLVBOS_H_
-#define WIREBOUNDWORLDCREATOR_SRC_VBOS_ALLVBOS_H_
-
-#include "UiDataInstanced.h"
-#include "UiDataMain.h"
-#include "UiDataText.h"
-
-#endif  // WIREBOUNDWORLDCREATOR_SRC_VBOS_ALLVBOS_H_
