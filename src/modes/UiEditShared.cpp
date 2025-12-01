11d34d3bd35820e0225395fb63ac6db33e7625ab
pol-31 <tsrkpashkevich@gmail.com>
2025-12-01 23:58:38 +0200

    add UiEditShared.h entries configuration for UiEdit* window to reuse in ui modes

---
+++ src/modes/UiEditShared.cpp
@@ -0,0 +1,227 @@
+#include "UiEditShared.h"
+
+#include "../core/Ui.h"
+
+UiEditConfigTerrain::UiEditConfigTerrain(
+    UiSharedResources& ui_shared_resources,
+    TextRenderer& text_renderer)
+    : btn_config_(data::VboIdMain::kTerrainEditNoiseConfig),
+      txt_name_(text_renderer,
+                {data::VboIdMain::kTerrainEditNoiseName},
+                data::TextId::kPerlin),
+      tg_invert_({data::VboIdMain::kTerrainEditNoiseInvertOff},
+                 {data::VboIdMain::kTerrainEditNoiseInvertOn1},
+                 {data::VboIdMain::kTerrainEditNoiseInvertOn2},
+                 {data::VboIdMain::kTerrainEditNoiseInvertOn3}),
+      tg_tiling_({data::VboIdMain::kTerrainEditNoiseTilingOff},
+                 {data::VboIdMain::kTerrainEditNoiseTilingOn1},
+                 {data::VboIdMain::kTerrainEditNoiseTilingOn2},
+                 {data::VboIdMain::kTerrainEditNoiseTilingOn3}),
+      sl_strength_({data::VboIdMain::kTerrainEditNoiseStrengthArea},
+                   {data::VboIdMain::kTerrainEditNoiseStrengthIcon}),
+      ui_shared_resources_(ui_shared_resources),
+      sp_hmap_(data::VboIdMain::kTerrainEditNoiseHmap) {}
+
+void UiEditConfigTerrain::ResetTransform() {
+  LocalTransform transform;
+  sp_hmap_.SetParentTransform(transform);
+  btn_config_.SetParentTransform(transform);
+  tg_invert_.SetParentTransform(transform);
+  tg_tiling_.SetParentTransform(transform);
+  sl_strength_.SetParentTransform(transform);
+  txt_name_.SetParentTransform(transform);
+}
+void UiEditConfigTerrain::Render(
+    float strength, bool do_invert, bool do_tiling,
+    glm::vec2 translate, bool update_strength, data::TextId text_id) {
+  LocalTransform transform;
+  transform.translate = translate;
+  sp_hmap_.SetParentTransform(transform);
+  btn_config_.SetParentTransform(transform);
+  tg_invert_.SetParentTransform(transform);
+  tg_tiling_.SetParentTransform(transform);
+  sl_strength_.SetParentTransform(transform);
+  txt_name_.SetParentTransform(transform);
+
+//  ui_shared_resources_.hmap_shader_.Bind();
+//  glBindTexture(GL_TEXTURE_2D, terrain_data->hmap_id);
+//  hmap_.Render();
+  ui_shared_resources_.dynamic_sprite_shader_.Bind();
+  ui_shared_resources_.tex_ui_.Bind();
+  btn_config_.Render();
+  tg_invert_.Set(do_invert);
+  tg_tiling_.Set(do_tiling);
+  tg_invert_.Render();
+  tg_tiling_.Render();
+  if (update_strength) {
+    auto mouse_pos =
+        ui_shared_resources_.global_glfw_callback_data_.cursor_pos_tex_norm_;
+    sl_strength_.Render(mouse_pos);
+    strength = sl_strength_.GetProgress();
+  } else {
+    sl_strength_.SetValue(strength);
+    sl_strength_.Render();
+  }
+  // only after (due to internal shader & texture modification)
+  txt_name_.SetText(text_id);
+  txt_name_.Render();
+}
+
+void UiEditConfigTerrain::RenderPicking(glm::vec2 translate) {
+  LocalTransform transform;
+  transform.translate = translate;
+  sp_hmap_.SetParentTransform(transform);
+  btn_config_.SetParentTransform(transform);
+  tg_invert_.SetParentTransform(transform);
+  tg_tiling_.SetParentTransform(transform);
+  sl_strength_.SetParentTransform(transform);
+  txt_name_.SetParentTransform(transform);
+  ui_shared_resources_.dynamic_sprite_picking_shader_.Bind();
+  btn_config_.RenderPicking();
+  tg_invert_.RenderPicking();
+  tg_tiling_.RenderPicking();
+  sl_strength_.RenderPicking();
+  sp_hmap_.RenderPicking();
+  // only after (due to internal shader & texture modification)
+  if (debug::gUiAltMode) {
+    txt_name_.RenderPicking();
+  }
+}
+
+void UiEditConfigTerrain::AttachToHierarchy(UiHierarchy& hierarchy) {
+  hierarchy.AddNested(
+      &btn_config_, &tg_invert_, &txt_name_,
+      &tg_tiling_, &sl_strength_, &sp_hmap_);
+}
+
+int GetUiEditEntryId(
+    const UiDynamicSprite* sprite, float size,
+    glm::vec2 mouse_pos, float start_height) {
+  float entry_height = sprite->GetHeight() / (size + 5);
+  float cursor = start_height;
+  // pretty heavy check on DBG, skip
+  //  float top = sprite_.GetTopBorder();
+  //  float bottom = sprite_.GetBottomBorder();
+  //  if (mouse_pos.y > top || mouse_pos.y < bottom) {
+  //    return -1;
+  //  }
+  for (int i = 0; i < size; ++i) {
+    cursor -= entry_height;
+    if (cursor < mouse_pos.y) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+UiEditConfigOcean::UiEditConfigOcean(
+    UiSharedResources& ui_shared_resources,
+    TextRenderer& text_renderer)
+    : btn_config_(data::VboIdMain::kWaterEditOceanConfig),
+      txt_name_(text_renderer,
+                {data::VboIdMain::kWaterEditOceanLayerName},
+                data::TextId::kPerlin),
+      tg_visible_({data::VboIdMain::kWaterEditOceanVisibleOff},
+                  {data::VboIdMain::kWaterEditOceanVisibleOn1},
+                  {data::VboIdMain::kWaterEditOceanVisibleOn2},
+                  {data::VboIdMain::kWaterEditOceanVisibleOn3}),
+      ui_shared_resources_(ui_shared_resources) {}
+
+void UiEditConfigOcean::ResetTransform() {
+  LocalTransform transform;
+  btn_config_.SetParentTransform(transform);
+  txt_name_.SetParentTransform(transform);
+  tg_visible_.SetParentTransform(transform);
+}
+void UiEditConfigOcean::Render(
+    bool do_show, glm::vec2 translate, data::TextId text_id) {
+  LocalTransform transform;
+  transform.translate = translate;
+  btn_config_.SetParentTransform(transform);
+  txt_name_.SetParentTransform(transform);
+  tg_visible_.SetParentTransform(transform);
+
+//  ui_shared_resources_.hmap_shader_.Bind();
+  ui_shared_resources_.dynamic_sprite_shader_.Bind();
+  ui_shared_resources_.tex_ui_.Bind();
+  btn_config_.Render();
+  tg_visible_.Set(do_show);
+  tg_visible_.Render();
+  txt_name_.SetText(text_id);
+  txt_name_.Render();
+}
+
+void UiEditConfigOcean::RenderPicking(glm::vec2 translate) {
+  LocalTransform transform;
+  transform.translate = translate;
+  btn_config_.SetParentTransform(transform);
+  txt_name_.SetParentTransform(transform);
+  tg_visible_.SetParentTransform(transform);
+  ui_shared_resources_.dynamic_sprite_picking_shader_.Bind();
+  btn_config_.RenderPicking();
+  tg_visible_.RenderPicking();
+  // only after (due to internal shader & texture modification)
+  if (debug::gUiAltMode) {
+    txt_name_.RenderPicking();
+  }
+}
+
+void UiEditConfigOcean::AttachToHierarchy(UiHierarchy& hierarchy) {
+  hierarchy.AddNested(&btn_config_, &txt_name_, &tg_visible_);
+}
+
+UiEditConfigSlTxt::UiEditConfigSlTxt(
+    UiSharedResources& ui_shared_resources,
+    TextRenderer& text_renderer,
+    UiSliderH2&& sl_strength_,
+    UiTextModeId&& txt_name)
+    : sl_strength_(std::move(sl_strength_)),
+      txt_name_(std::move(txt_name)),
+      ui_shared_resources_(ui_shared_resources) {}
+
+void UiEditConfigSlTxt::ResetTransform() {
+  LocalTransform transform;
+  sl_strength_.SetParentTransform(transform);
+  txt_name_.SetParentTransform(transform);
+}
+void UiEditConfigSlTxt::Render(
+    float strength, // viscosity / transparency
+    glm::vec2 translate,
+    bool update_strength, data::TextId text_id) {
+  LocalTransform transform;
+  transform.translate = translate;
+  sl_strength_.SetParentTransform(transform);
+  txt_name_.SetParentTransform(transform);
+
+//  ui_shared_resources_.hmap_shader_.Bind();
+  ui_shared_resources_.dynamic_sprite_shader_.Bind();
+  ui_shared_resources_.tex_ui_.Bind();
+  if (update_strength) {
+    auto mouse_pos =
+        ui_shared_resources_.global_glfw_callback_data_.cursor_pos_tex_norm_;
+    sl_strength_.Render(mouse_pos);
+    strength = sl_strength_.GetProgress();
+  } else {
+    sl_strength_.SetValue(strength);
+    sl_strength_.Render();
+  }
+  txt_name_.SetText(text_id);
+  txt_name_.Render();
+}
+
+void UiEditConfigSlTxt::RenderPicking(glm::vec2 translate) {
+  LocalTransform transform;
+  transform.translate = translate;
+  sl_strength_.SetParentTransform(transform);
+  txt_name_.SetParentTransform(transform);
+  ui_shared_resources_.dynamic_sprite_picking_shader_.Bind();
+  sl_strength_.RenderPicking();
+  // only after (due to internal shader & texture modification)
+  if (debug::gUiAltMode) {
+    txt_name_.RenderPicking();
+  }
+}
+
+void UiEditConfigSlTxt::AttachToHierarchy(UiHierarchy& hierarchy) {
+  hierarchy.AddNested(&sl_strength_, &txt_name_);
+}
---
+++ src/modes/UiEditShared.h
@@ -0,0 +1,114 @@
+#ifndef WIREBOUNDWORLDCREATOR_SRC_MODES_UIEDITSHARED_H_
+#define WIREBOUNDWORLDCREATOR_SRC_MODES_UIEDITSHARED_H_
+
+#include <glm/glm.hpp>
+
+#include "../core/Ui.h"
+#include "../core/UiComplex.h"
+#include "../core/UiText.h"
+#include "UiSharedResources.h"
+
+class UiDynamicSprite;
+
+int GetUiEditEntryId(const UiDynamicSprite* sprite, float size,
+                     glm::vec2 mouse_pos, float start_height);
+
+class UiEditConfigTerrain {
+ public:
+  UiEditConfigTerrain(
+      UiSharedResources& ui_shared_resources,
+      TextRenderer& text_renderer);
+
+  UiEditConfigTerrain(UiEditConfigTerrain&& other) noexcept = default;
+  UiEditConfigTerrain(const UiEditConfigTerrain& other) = delete;
+
+  UiEditConfigTerrain& operator=(UiEditConfigTerrain&& other) = delete;
+  UiEditConfigTerrain& operator=(const UiEditConfigTerrain& other) = delete;
+
+  /// so we could get GetTopBorder & GetBottomBorder and estimate position
+  void ResetTransform();
+
+  /// no Press(), Release() <- done in external ui_event_handler
+  void Render(float strength, bool do_invert, bool do_tiling,
+              glm::vec2 translate, bool update_strength, data::TextId text_id);
+
+  void RenderPicking(glm::vec2 translate);
+
+  void AttachToHierarchy(UiHierarchy& hierarchy);
+
+  UiDynamicSprite btn_config_;
+  UiTextModeId txt_name_;
+  UiToggle4 tg_invert_;
+  UiToggle4 tg_tiling_;
+  UiSliderH2 sl_strength_;
+  UiDynamicSprite sp_hmap_;
+
+  int pressed_strength_id_ = -1.0f;
+
+  UiSharedResources& ui_shared_resources_;
+};
+
+class UiEditConfigOcean {
+ public:
+  UiEditConfigOcean(
+      UiSharedResources& ui_shared_resources,
+      TextRenderer& text_renderer);
+
+  UiEditConfigOcean(UiEditConfigOcean&& other) noexcept = default;
+  UiEditConfigOcean(const UiEditConfigOcean& other) = delete;
+
+  UiEditConfigOcean& operator=(UiEditConfigOcean&& other) = delete;
+  UiEditConfigOcean& operator=(const UiEditConfigOcean& other) = delete;
+
+  /// so we could get GetTopBorder & GetBottomBorder and estimate position
+  void ResetTransform();
+
+  /// no Press(), Release() <- done in external ui_event_handler
+  void Render(bool do_show, glm::vec2 translate,
+              data::TextId text_id);
+
+  void RenderPicking(glm::vec2 translate);
+
+  void AttachToHierarchy(UiHierarchy& hierarchy);
+
+  UiDynamicSprite btn_config_;
+  UiTextModeId txt_name_;
+  UiToggle4 tg_visible_;
+
+  UiSharedResources& ui_shared_resources_;
+};
+
+class UiEditConfigSlTxt {
+ public:
+  UiEditConfigSlTxt(
+      UiSharedResources& ui_shared_resources,
+      TextRenderer& text_renderer,
+      UiSliderH2&& sl_strength_,
+      UiTextModeId&& txt_name_);
+
+  UiEditConfigSlTxt(UiEditConfigSlTxt&& other) noexcept = default;
+  UiEditConfigSlTxt(const UiEditConfigSlTxt& other) = delete;
+
+  UiEditConfigSlTxt& operator=(UiEditConfigSlTxt&& other) = delete;
+  UiEditConfigSlTxt& operator=(const UiEditConfigSlTxt& other) = delete;
+
+  /// so we could get GetTopBorder & GetBottomBorder and estimate position
+  void ResetTransform();
+
+  /// no Press(), Release() <- done in external ui_event_handler
+  void Render(float strength, glm::vec2 translate,
+              bool update_strength, data::TextId text_id);
+
+  void RenderPicking(glm::vec2 translate);
+
+  void AttachToHierarchy(UiHierarchy& hierarchy);
+
+  UiSliderH2 sl_strength_;
+  UiTextModeId txt_name_;
+
+  int pressed_strength_id_ = -1.0f;
+
+  UiSharedResources& ui_shared_resources_;
+};
+
+#endif  // WIREBOUNDWORLDCREATOR_SRC_MODES_UIEDITSHARED_H_
